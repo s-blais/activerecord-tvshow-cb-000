a9a8ca4ca01693d26738e27a55eeed6c70228de2
Stephen Blais <stephen@s-blais.com>
2019-10-15 21:05:26 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,20 @@
 class Show < ActiveRecord::Base
 
   def self.highest_rating
-    self.maximum(:rating)
+    self.maximum(:rating).rating
+  end
+
+  def self.most_popular_show
+    self.maximum(:rating).name
+  end
+
+  def lowest_rating
+    self.minimum(:rating).rating
+  end
+
+  def least_popular_show
+    self.minimum(:rating).name
   end
   
+
 end
