ffc7ec1d879ab13040306b88996cadac4106aa00
Stephen Blais <stephen@s-blais.com>
2019-10-15 21:23:43 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -16,5 +16,16 @@ class Show < ActiveRecord::Base
     self.where("rating = #{self.lowest_rating}").[0]
   end
 
+  def self.ratings_sum
+    self.sum(:rating)
+  end
+
+  def self.popular_shows
+    self.where("rating > 5")
+  end
+
+  def self.shows_by_alphabetical_order
+    self.order(name: :asc)
+  end
 
 end
